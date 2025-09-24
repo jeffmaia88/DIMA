846370b64380a49aca6c737f6ff2dd98418a802f
Jefferson Maia <jefferson.reis88@gmail.com>
2025-09-24 19:33:47 -0300

    Created Request / Responses

---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Requests
+{
+   public class PagedRequest : Request
+    {
+        public int PageNumber { get; set; } = Configurations.DefaultPageNumber;
+        public int PageSize { get; set; } = Configurations.DefaultPageSize;
+    }
+}
---
+++ Dima.Core/Responses/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Responses
+{
+    public class Response<TData>
+    {
+        private readonly int _code = Configurations.DefaultStatusCode;
+
+        [JsonConstructor]
+        public Response()
+        {
+            _code = Configurations.DefaultStatusCode;
+        }
+
+        public Response(TData? data,int code = 200, string? message = null)
+        {
+            Data = data;
+            _code = Configurations.DefaultStatusCode;
+            Message = message;
+            
+        }
+
+        public TData? Data { get; set; }
+        public string? Message { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public bool IsSuccess => _code is >= 200 and <= 299;
+        
+
+    }
+}
